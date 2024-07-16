0e382dedd424b645bd68b42faded69f1ed380f17
Arcane_Stone <140941764+ArcaneStone@users.noreply.github.com>
2024-07-16 12:04:50 +0300

    2.cs

---
+++ Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5f;
+
+    private Vector3 _direction;
+
+    private void Update()
+    {
+        transform.position += _direction * _speed * Time.deltaTime;
+    }
+
+    public void SetMovementDirection(Vector3 direction)
+    {
+        _direction = direction;
+    }
+}
